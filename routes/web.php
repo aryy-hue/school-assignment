3716c815afdc815723a8a97fdc0f81b508368211
wardhana2451@gmail.com <Arya>
2022-01-26 16:02:11 +0700

    add form

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.dashboard');
 });
+Route::get('/form', function () {
+    return view('pages.form');
+})->name('form');
